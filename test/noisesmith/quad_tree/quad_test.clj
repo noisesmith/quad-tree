5eec342f1186c9928dd4348dc641c2290157711f
Justin Smith <noisesmith@gmail.com>
2013-11-11 14:56:35 -0800

    first unit test passing

--- test/noisesmith/quad_tree/quad_test.clj
+++ test/noisesmith/quad_tree/quad_test.clj
@@ -1,5 +1,15 @@
 (ns noisesmith.quad-tree.quad-test
   (:require [clojure.test :as test :refer [deftest testing is]]
-            [quad-tree.quad :as quad]))
+            [noisesmith.quad-tree.quad :as quad]))
 
-
+(deftest make-quad
+  (is (= (into {} (quad/make-quad [0 0] [1 1]))
+         {:ne nil
+          :se nil
+          :sw nil
+          :nw nil
+          :x 0
+          :y 0
+          :x' 1
+          :y' 1
+          :properties #{}})))
